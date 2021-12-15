4671b8574c91705e5f40dfed6548a6bd79b6175e
Chakravarthi Medicharla <medicharlachakravarthi@gmail.com>
2021-12-15 18:55:44 +0530

    Config check

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
+const URI = process.env.MONGO_URI;
+
 app.get("/", (req, res) => {
   res.send("<h1>Docker Compose</h1>");
 });
 
-app.listen(PORT, () => {
-  console.log(`the app is listening at ${PORT}`);
-});
+const connectToDB = async () => {
+  await mongoose.connect(URI, () => {
+    console.log("This is Connected to the Database");
+  });
+};
+
+const start = () => {
+  try {
+    connectToDB();
+    app.listen(PORT, () => {
+      console.log(`the app is listening at ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
