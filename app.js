1b921448dcfc138d1c5e46b79d3fe9e1a9e1c91e
Chakravarthi Medicharla <medicharlachakravarthi@gmail.com>
2021-12-17 20:35:05 +0530

    Redis setup

--- app.js
+++ app.js
@@ -3,7 +3,18 @@ const app = express();
 const mongoose = require("mongoose");
 const blogRouter = require("./Routes/routes");
 const userRouter = require("./Routes/UserRoutes");
+const redis = require("redis");
+const session = require("express-session");
 require("dotenv").config();
+
+//get the redis store
+
+let RedisStore = require("redis-connect")(session);
+//setting up the redis clinet
+let redisClient = redis.createClient({
+  host: "redis",
+  port: 6379,
+});
 
 const PORT = process.env.PORT || 3000;
 const URI = process.env.MONGO_URI;
@@ -16,6 +27,15 @@ app.use(express.json());
 
 app.use("/home", blogRouter);
 app.use("/home", userRouter);
+
+app.use(
+  session({
+    store: new RedisStore({ client: redisClient }),
+    saveUninitialized: false,
+    secret: "keyboard cat",
+    resave: false,
+  })
+);
 
 const connectToDB = async () => {
   await mongoose.connect(URI, { useNewUrlParser: true });
