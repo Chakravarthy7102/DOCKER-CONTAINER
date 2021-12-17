78f3be1562cf813ccc0432278a4e438534379dfa
Chakravarthi Medicharla <medicharlachakravarthi@gmail.com>
2021-12-17 18:50:22 +0530

    Impleted User Login and Authentication

--- Controller/authController.js
+++ Controller/authController.js
@@ -1,10 +1,17 @@
 const UserModel = require("../Model/userModel");
+const bcryptjs = require("bcryptjs");
 
 //controller for the user model auth
 
 exports.createUser = async (req, res) => {
+  const { user, password } = req.body;
+
   try {
-    const newuser = await UserModel.create(req.body);
+    const hashPassword = await bcryptjs.hash(password, 12);
+    const newuser = await UserModel.create({
+      user,
+      password: hashPassword,
+    });
     res.status(200).json({
       newuser,
       msg: "the new User is Created !",
@@ -12,6 +19,38 @@ exports.createUser = async (req, res) => {
   } catch (error) {
     res.status(500).json({
       msg: "SomeHow the req has Failed",
+      error,
+    });
+  }
+};
+
+exports.login = async (req, res) => {
+  const { user, password } = req.body;
+
+  try {
+    const userInDatabase = await UserModel.findOne({ user });
+
+    if (!userInDatabase) {
+      return res.status(404).json({
+        msg: "User not Found!",
+      });
+    }
+
+    const isCorrect = await bcryptjs.compare(password, userInDatabase.password);
+
+    if (isCorrect) {
+      res.status(200).json({
+        msg: "User has Logged In!",
+      });
+    } else {
+      res.status(400).json({
+        msg: "Check your password and the UserName",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      msg: "SomeHow the req has Failed",
+      error,
     });
   }
 };
--- Routes/UserRoutes.js
+++ Routes/UserRoutes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const authController = require("../Controller/authController");
 
 router.route("/signup").post(authController.createUser);
+router.route("/login").get(authController.login);
 
 module.exports = router;
